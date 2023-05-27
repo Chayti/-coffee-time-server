7d38c26bc2e4e9c51163882558a40f72796172f6
Chayti Saha <chaytisaha98@gmail.com>
2023-05-28 00:43:15 +0600

    deployed to vercel

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ const ObjectId = require('mongodb').ObjectId;
 const stripe = require('stripe')(process.env.STRIPE_SECRET)
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors())
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iea9q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1, });
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
         const database = client.db("coffeTime");
         const productCollection = database.collection("products");
         const orderCollection = database.collection("orders");
